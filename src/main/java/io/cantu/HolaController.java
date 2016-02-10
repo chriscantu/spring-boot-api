294f60c4e964353bd3102ecf270dacde59232399
Chris Cantu <chris.cantu@allclearid.com>
2016-02-09 22:23:45 -0600

    Added empty Hola Controller

---
+++ src/main/java/io/cantu/HolaController.java
@@ -0,0 +1,12 @@
+package io.cantu;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/hola")
+public class HolaController {
+    
+}
